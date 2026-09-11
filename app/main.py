3078f1c74d9e86aaa543b882cb0453e2c12f16ae
Khang <ankhangbc.2021@gmail.com>
2026-09-11 09:17:00 +0700

    Allow OPTIONS method in CORS middleware
    Added OPTIONS method to CORS middleware configuration.

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app = FastAPI(
     title=settings.APP_NAME,
     description=settings.APP_DESCRIPION,
     version=settings.APP_VERSION,
+    servers=[{"url": "/", "description": "Default Server"}],
 )
 
 origins_whitelist = settings.ALLOWED_ORIGINS
@@ -38,7 +39,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins_whitelist,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Authorization"],
 )
 
